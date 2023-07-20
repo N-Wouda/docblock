8822518030708e9a2798fb27e182960ef32f5183
Niels Wouda <nielswouda@gmail.com>
2023-07-20 09:18:01 +0200

    Add clang-format, failing test

---
+++ docblock/tests/examples/namespace.h
@@ -0,0 +1,13 @@
+// Simple namespace example. This tests if the function documentation for
+// test::Test::test is parsed correctly.
+
+namespace test
+{
+class Test
+{
+    /**
+     * Test docstring
+     */
+    void test() const;
+}
+};  // namespace test
--- docblock/tests/test_parse.py
+++ docblock/tests/test_parse.py
@@ -1,5 +1,11 @@
-from docblock import parse
+from docblock import parse, parse_file
 
 
 def test_parse_empty_string_returns_empty():
     assert parse("") == {}
+
+
+def test_namespace():
+    expected = {"test::Test::test": "Test docstring"}
+
+    assert parse_file("docblock/tests/examples/namespace.h") == expected
